3ff8adfbc2443e6685f3ce620d16fa0907c14c3c
FIRST_NAME LAST_NAME <sander.eikenes2003@gmail.com>
2025-02-14 13:31:46 +0100

    testcommit

--- src/github_utils.py
+++ src/github_utils.py
@@ -1,7 +1,9 @@
 from github import PullRequest, InputGitAuthor, Repository
 
 def get_latest_commit_message(pull_request : PullRequest) -> list[str]:
-    raise NotImplementedError("TODO")
+    for commit in pull_request.get_commits():
+        print(commit.commit.message)
+        return commit.commit.message
 
 def commit_and_push(repo: Repository, target_branch:str, file_path: str) -> None:
     author = InputGitAuthor(
@@ -12,3 +14,5 @@ def commit_and_push(repo: Repository, target_branch:str, file_path: str) -> None
         new_file_content = f.read()
 
     ##TODO use update_file from github.Repository to push the new file contents to the open PR 
+
+get_latest_commit_message(PullRequest)
